ed2d6fd65c5f87cc7bd13ca0d956a20f2bdcaf42
AInstAIn <ainstain2025@gmail.com>
2026-07-26 14:07:50 +0200

    Add maxDuration to edge runtime configuration

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export const config = { runtime: 'edge' };
+export const config = { runtime: 'edge', maxDuration: 60 };
 
 import { jwtVerify } from 'jose';
 import { freeDailyLimiter, abuseLimiter, getClientIp } from '../lib/rateLimit.js';
